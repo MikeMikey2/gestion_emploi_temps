45848fe831ee7f96dc100005d5605814fedb25fc
MikeMikey2 <trafalgardwhaterlaw87@gmail.com>
2026-02-26 20:04:10 +0100

    mise en place de genpdf

---
+++ Teachers/genpdf.php
@@ -0,0 +1,9 @@
+<?php
+use Dompdf\Dompdf;
+require_once "../dompdf/autoload.inc.php";
+$dompdf = new Dompdf();
+$dompdf->loadHtml('<h1>Bonjour, ceci est un PDF généré par Dompdf !</h1>');
+$dompdf->setPaper('A4', 'portrait');
+$dompdf->render();
+$dompdf->stream();
+?>
