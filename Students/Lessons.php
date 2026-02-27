bf2c510f116ca33134daa004a96666ff67a296d3
MikeMikey2 <trafalgardwhaterlaw87@gmail.com>
2026-02-27 22:06:23 +0100

    Ajout Lesson

--- Students/Lessons.php
+++ Students/Lessons.php
@@ -5,8 +5,8 @@ if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'enseignant') {
     exit; 
 }
 include_once "../ADMIN/con_dbb.php";
-if(isset($_POST['btn'])) {
-    header("Location: pdf-content.php");
+if(isset($_POST['generate_pdf'])) {
+    header("Location: ../Teachers/genpdf.php");
     exit();
 }
 ?>
@@ -24,8 +24,7 @@ if(isset($_POST['btn'])) {
      <h5 class="menu">MENU</h5>
          <ul class="nav-list">
             <li><a href="Emploi.php" class="<?= (basename($_SERVER['PHP_SELF'])=='Emploi.php') ? 'nav-active' : '' ?>"><img src="../icons/evenement.png" alt="20" width="30"> Emploi du temps</a></li>
-            <li><a href="Requetes.php" class="<?= (basename($_SERVER['PHP_SELF'])=='Requetes.php') ? 'nav-active' : '' ?>"><img src="../icons/message.jpeg" alt="20" width="30">Requetes <span class="badge"><?php echo mysqli_num_rows(mysqli_query($con, "SELECT * FROM REQUETE WHERE statut='acceptée' OR statut='refusée'")); ?></span></a></li>
-            <li><a href="Leçons.php" class="<?= (basename($_SERVER['PHP_SELF'])=='Leçons.php') ? 'nav-active' : '' ?>"><img src="../icons/prof.png" alt="20" width="30">Leçons</a></li>
+            <li><a href="Lesson.php" class="<?= (basename($_SERVER['PHP_SELF'])=='Lesson.php') ? 'nav-active' : '' ?>"><img src="../icons/prof.png" alt="20" width="30">Leçons</a></li>
             <li><a href="../logout.php" class="<?= (basename($_SERVER['PHP_SELF'])=='../logout.php') ? 'nav-active' :'' ?>"><img src="../icons/back.jpeg" alt="20" width="30">Deconnexion</a></li>
         </ul>
     </nav>
@@ -33,7 +32,7 @@ if(isset($_POST['btn'])) {
         <h1>Mes leçons</h1>
         <p>Contenu des leçons à venir...</p>
         <form action="#" method="post">
-            <button class="btn-primary" name="btn">Envoyer</button>
+            <button class="btn-primary" name="generate_pdf">Générer PDF</button>
         </form>
     </section>
 </body>
