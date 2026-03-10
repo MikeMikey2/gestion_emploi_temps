1724d3cc3248d78f6b363ff910d9e9e0ee5c4417
MikeMikey2 <trafalgardwhaterlaw87@gmail.com>
2026-03-10 14:28:12 +0100

    icone

--- ADMIN/Emploi.php
+++ ADMIN/Emploi.php
@@ -19,7 +19,7 @@
             <li><a href="Gestion.php" class="<?= (basename($_SERVER['PHP_SELF'])=='Gestion.php') ? 'nav-active' : '' ?>"><img src="../icons/per.png" alt="20" width="30">Gestion</a></li>
             <li><a href="Emploi.php" class="<?= (basename($_SERVER['PHP_SELF'])=='Emploi.php') ? 'nav-active' : '' ?>"><img src="../icons/evenement.png" alt="20" width="30">Emploi du temps</a></li>
             <li><a href="Requetes.php" class="<?= (basename($_SERVER['PHP_SELF'])=='Requetes.php') ? 'nav-active' : '' ?>"><img src="../icons/message.jpeg" alt="20" width="30">Requetes<span class="badge"><?php echo mysqli_num_rows(mysqli_query($con, "SELECT * FROM REQUETE WHERE statut='en_attente'")); ?></span></a></li>
-            <li><a href="profdispo.php" class="<?= (basename($_SERVER['PHP_SELF'])=='profdispo.php') ? 'nav-active' : '' ?>"><img src="../icons/calendar.png" alt="20" width="30">Enseignants disponibles</a></li>
+            <li><a href="profdispo.php" class="<?= (basename($_SERVER['PHP_SELF'])=='profdispo.php') ? 'nav-active' : '' ?>"><img src="../icons/dispo.jpeg" alt="20" width="30">Enseignants disponibles</a></li>
             <li><a href="../logout.php" class="<?= (basename($_SERVER['PHP_SELF'])=='../logout.php') ? 'nav-active' :'' ?>"><img src="../icons/back.jpeg" alt="20" width="30">Deconnexion</a></li>
         </ul>
     </nav>
--- ADMIN/profdispo.php
+++ ADMIN/profdispo.php
@@ -5,10 +5,10 @@ session_start();
 
 
 $stmt = $con->prepare("SELECT d.*,p.nom,p.prenom FROM DISPONIBILITE d JOIN PERSONNE p ON d.id_personne = p.id_personne WHERE p.enseignant = 1  ORDER BY d.date, d.heure");
-$stmt->bind_param("i", $_SESSION['id_personne']);
 $stmt->execute();
 $result = $stmt->get_result();
 foreach($result as $row) {
+
 
 ?>
 <!DOCTYPE html>
@@ -28,7 +28,7 @@ foreach($result as $row) {
             <li><a href="Gestion.php" class="<?= (basename($_SERVER['PHP_SELF'])=='Gestion.php') ? 'nav-active' : '' ?>"><img src="../icons/per.png" alt="20" width="30">Gestion</a></li>
             <li><a href="Emploi.php" class="<?= (basename($_SERVER['PHP_SELF'])=='Emploi.php') ? 'nav-active' : '' ?>"><img src="../icons/evenement.png" alt="20" width="30">Emploi du temps</a></li>
             <li><a href="Requetes.php" class="<?= (basename($_SERVER['PHP_SELF'])=='Requetes.php') ? 'nav-active' : '' ?>"><img src="../icons/message.jpeg" alt="20" width="30">Requetes<span class="badge"><?php echo mysqli_num_rows(mysqli_query($con, "SELECT * FROM REQUETE WHERE statut='en_attente'")); ?></span></a></li>
-            <li><a href="profdispo.php" class="<?= (basename($_SERVER['PHP_SELF'])=='profdispo.php') ? 'nav-active' : '' ?>"><img src="../icons/calendar.png" alt="20" width="30">Enseignants disponibles</a></li>
+            <li><a href="profdispo.php" class="<?= (basename($_SERVER['PHP_SELF'])=='profdispo.php') ? 'nav-active' : '' ?>"><img src="../icons/dispo.jpeg" alt="20" width="30">Enseignants disponibles</a></li>
             <li><a href="../logout.php" class="<?= (basename($_SERVER['PHP_SELF'])=='../logout.php') ? 'nav-active' :'' ?>"><img src="../icons/back.jpeg" alt="20" width="30">Deconnexion</a></li>
         </ul>
     </nav>
